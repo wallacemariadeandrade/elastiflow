b6d03c6ae97b94d880f720bd278414638d1a99aa
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2025-05-04 15:41:12 -0300

    updates scrpt

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo 'vm.max_map_count=262144' | tee -a /etc/sysctl.conf
+sysctl -p
 docker compose create
 cp GeoLite2-ASN.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-City.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
@@ -8,3 +10,4 @@ docker compose up -d
 echo "Waiting for elastiflow to be up..."
 sleep 30
 curl -XPOST "http://127.0.0.1:5601/api/saved_objects/_import?overwrite=true" -k -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@kibana-8.2.x-flow-codex.ndjson
+echo "Finished!"
