82d495bb2dc5770d5fbcff71d42e9468214a0c8a
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2025-05-04 15:52:35 -0300

    update script

--- install.sh
+++ install.sh
@@ -7,7 +7,5 @@ cp GeoLite2-City.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-Country.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 docker compose up -d
 
-echo "Waits 60s for elastiflow to be up..."
-sleep 60
-curl -XPOST "http://127.0.0.1:5601/api/saved_objects/_import?overwrite=true" -k -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@kibana-8.2.x-flow-codex.ndjson
-echo "Finished!"
+echo "After server is running, run this command to add default dashboards:"
+echo "curl -XPOST "http://127.0.0.1:5601/api/saved_objects/_import?overwrite=true" -k -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@kibana-8.2.x-flow-codex.ndjson"
