f0f379f3e20de1fea6d03c28cba5bfad1ba6b5a9
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2025-05-04 15:43:36 -0300

    updates script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp GeoLite2-City.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-Country.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 docker compose up -d
 
-echo "Waiting for elastiflow to be up..."
-sleep 30
+echo "Waits 60s for elastiflow to be up..."
+sleep 60
 curl -XPOST "http://127.0.0.1:5601/api/saved_objects/_import?overwrite=true" -k -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@kibana-8.2.x-flow-codex.ndjson
 echo "Finished!"
