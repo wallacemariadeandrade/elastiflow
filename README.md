0d24539673fe0a229b68c22071a7c2c810754702
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2025-05-15 18:41:33 -0300

    update infos

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # elastiflow
 Runs Elastiflow in Docker
 
+- Set `vm.max_map_count` with `echo 'vm.max_map_count=262144' | tee -a /etc/sysctl.conf` in order to get services running properly.
 - Netflow port is 9996 by default
 - Use override file to enable manual sampler definitions. You'll also need to update sample_rate.yml inside flowcoll settings.
+- To import default dashboard, use `curl -XPOST "http://127.0.0.1:5601/api/saved_objects/_import?overwrite=true" -k -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@kibana-8.2.x-codex-dark.ndjson`
 
 ## references
 
--- install.sh
+++ install.sh
@@ -2,15 +2,11 @@
 echo "####################"
 echo "#    ATTENTION     #"
 echo "####################"
-echo "You need to set 'vm.max_map_count=262144 in sysctl.conf'. Use these comands:"
-echo "vm.max_map_count=262144' | tee -a /etc/sysctl.conf"
-echo "sysctl -p"
+echo "You need to set 'vm.max_map_count=262144 in sysctl.conf'. Please read README.md for more info."
+echo "To import defaul dashboard, please read README.md file."
 
 docker compose create
 cp GeoLite2-ASN.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-City.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-Country.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 docker compose up -d
-
-echo "After server is running, run this command to add default dashboards:"
-echo "curl -XPOST "http://127.0.0.1:5601/api/saved_objects/_import?overwrite=true" -k -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@kibana-8.2.x-flow-codex.ndjson"
