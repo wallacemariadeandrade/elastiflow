469bc6c06cb0c8f88e95c1e3c00c2f7d69f47cbf
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2025-05-23 17:05:16 -0300

    add doc

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "####################"
-echo "#    ATTENTION     #"
-echo "####################"
-echo "You need to set 'vm.max_map_count=262144 in sysctl.conf'. Please read README.md for more info."
-echo "To import defaul dashboard, please read README.md file."
-
-docker compose create
+docker compose up --no-start
 cp GeoLite2-ASN.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-City.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
 cp GeoLite2-Country.mmdb /var/lib/docker/volumes/elastiflow_elastiflow/_data/
-docker compose up -d
+
+echo "####################"
+echo "#    ATTENTION     #"
+echo "####################"
+echo "Before run, you need to set 'vm.max_map_count=262144 in sysctl.conf'. Please read README.md for more info."
+echo "####################"
+echo "#    ATTENTION     #"
+echo "####################"
+echo "To import defaul dashboard, please read README.md file."
